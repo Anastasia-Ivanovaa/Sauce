03f949eb20c9de75bad1fcf1240f3b2a9f778c69
Anastasia Ivanova <anastasiaroubo@gmail.com>
2024-12-10 21:54:00 +0300

    dev: add log methods to Login page

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,9 +1,11 @@
 package pages;
 
 import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+@Log4j2
 public class LoginPage extends BasePage {
 
     public LoginPage(WebDriver driver) {
@@ -17,11 +19,13 @@ public class LoginPage extends BasePage {
 
     @Step("Open Login page")
     public void open() {
+        log.info("Accounts page is opened");
         driver.get("https://www.saucedemo.com/");
     }
 
     @Step("Log into system using {user} and password {password}")
     public void login(String user, String password) {
+        log.info("Log into application using credentials '{}' '{}'", user, password);
         driver.findElement(USER_INPUT).sendKeys(user);
         driver.findElement(PASSWORD_INPUT).sendKeys(password);
         driver.findElement(LOGIN_BUTTON).click();
@@ -29,6 +33,7 @@ public class LoginPage extends BasePage {
 
     @Step("Get the error message")
     public String getErrorMessage() {
+        log.info("Get the error message");
         return driver.findElement(ERROR_MESSAGE).getText();
     }
 }
